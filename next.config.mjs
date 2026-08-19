4a566aceb238631b76b3a1a29603fc0f251592a1
Evan <128929890+Evaxle@users.noreply.github.com>
2026-08-19 17:55:00 -0400

    fix: remove invalid archiver externalization

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  serverExternalPackages: ["archiver"],
   images: {
     remotePatterns: [
       {
