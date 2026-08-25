0ec97b2309951c07e3990c9bd53ee7684dc428cd
Evan <128929890+Evaxle@users.noreply.github.com>
2026-08-25 18:09:18 -0400

    Add dedicated community settings page

---
+++ app/community-settings/page.tsx
@@ -0,0 +1,7 @@
+import { SettingsShowcase } from "@/components/settings-showcase";
+
+export const dynamic = "force-dynamic";
+
+export default function CommunitySettingsPage() {
+  return <SettingsShowcase />;
+}
