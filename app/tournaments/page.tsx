143b01e90709d16747390940cf921d7b28ec962e
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-06 18:23:32 -0400

    add tournaments page

---
+++ app/tournaments/page.tsx
@@ -0,0 +1,7 @@
+import { TournamentsApp } from "@/components/tournaments/tournaments-app";
+
+export const dynamic = "force-dynamic";
+
+export default function TournamentsPage() {
+  return <TournamentsApp />;
+}
