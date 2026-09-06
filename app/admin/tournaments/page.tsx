90da8bb972e337367c9155ce0c549651a7bbd2f2
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-06 18:24:55 -0400

    add tournament admin page

---
+++ app/admin/tournaments/page.tsx
@@ -0,0 +1,7 @@
+import { TournamentAdmin } from "@/components/admin/tournament-admin";
+
+export const dynamic = "force-dynamic";
+
+export default function AdminTournamentsPage() {
+  return <TournamentAdmin />;
+}
