9ad93d49c4541101b8393ca0774707b475664e4a
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-07 16:46:02 -0400

    Show ready check for all battles

--- app/battles/match/[matchId]/page.tsx
+++ app/battles/match/[matchId]/page.tsx
@@ -1,5 +1,6 @@
 import { BattleChat } from "@/components/battles/battle-chat";
 import { BattleMatchApp } from "@/components/battles/battle-match-app";
+import { BattleReadyPanel } from "@/components/battles/battle-ready-panel";
 import { RbpBattleOverlay } from "@/components/battles/rbp-battle-overlay";
 import { getSessionUser } from "@/lib/auth";
 
@@ -9,5 +10,5 @@ export default async function BattleMatchPage({ params }: { params: Promise<{ ma
   const { matchId } = await params;
   const user = await getSessionUser();
   if (!user) return null;
-  return <div className="space-y-5"><BattleMatchApp matchId={matchId} /><RbpBattleOverlay matchId={matchId} /><BattleChat matchId={matchId} currentUserId={user.id} /></div>;
+  return <div className="space-y-5"><BattleMatchApp matchId={matchId} /><BattleReadyPanel matchId={matchId} /><RbpBattleOverlay matchId={matchId} /><BattleChat matchId={matchId} currentUserId={user.id} /></div>;
 }
