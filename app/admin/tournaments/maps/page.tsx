8c597a377b6a6f03ba41d7b59bc5e875b690ce6b
Evan <128929890+Evaxle@users.noreply.github.com>
2026-09-06 23:48:16 -0400

    Add tournament map pool admin page

---
+++ app/admin/tournaments/maps/page.tsx
@@ -0,0 +1,7 @@
+import { TournamentMapPoolAdmin } from "@/components/admin/tournament-map-pool-admin";
+
+export const dynamic = "force-dynamic";
+
+export default function TournamentMapPoolsPage() {
+  return <TournamentMapPoolAdmin />;
+}
